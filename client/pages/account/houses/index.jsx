40835ba0282652ae7c703d6ec2b0e69081c4bdf7
John Stoecker <johnstoecker@gmail.com>
2017-07-16 22:51:20 -0700

    update house page

--- client/pages/account/houses/index.jsx
+++ client/pages/account/houses/index.jsx
@@ -52,20 +52,20 @@ class HouseStatsPage extends React.Component {
                     </div>
                 </div>
                 <div className="col-sm-12 house-picker-container">
-                    <div className="house-picker-wrapper house-stats-wrapper greyjoy">
+                    <div className="house-picker-wrapper house-stats-wrapper lannister">
                         <div className="house-banner">
                             <img className="house-picker-image" src={"/public/media/tag_images/"+Houses[0].image} />
-                            <div className="house-banner-name">House Greyjoy</div>
+                            <div className="house-banner-name">House Lannister</div>
                         </div>
-                        <div className="house-attribute-title">⚓ We Do Not Sow ⚓</div>
-                        <div className="house-attribute-detail">+10 for each naval battle (2+ ships)</div>
-                        <div className="house-attribute-title">🐙 What is dead, could die 🐙</div>
-                        <div className="house-attribute-detail">-2 For each week no Greyjoy is seen on a ship</div>
+                        <div className="house-attribute-title">👑 Golden Crown 👑</div>
+                        <div className="house-attribute-detail">+6 for each week Cersei is Queen</div>
+                        <div className="house-attribute-title">👫 Twincest 👫</div>
+                        <div className="house-attribute-detail">-2 for each week Cersei and Jaime don't meet</div>
                         <div className="house-attribute-stats-container">
                             <div className="house-attribute-title-center-underline">Stats</div>
                             <div className="house-attribute-stats-box">
                                 <div className="house-attribute-stats">
-                                    <div className="house-attribute-stat">+{this.state.houseStats.data[0] && this.state.houseStats.data[0].plusCoins}/-{this.state.houseStats.data[0] && this.state.houseStats.data[0].minusCoins}</div>
+                                    <div className="house-attribute-stat">+{this.state.houseStats.data[0] && this.state.houseStats.data[0].plusCoins}/-{this.state.houseStats.data[1] && this.state.houseStats.data[1].minusCoins}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].userCount}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].coins}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].availableCoins}</div>
@@ -91,20 +91,21 @@ class HouseStatsPage extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="house-picker-wrapper house-stats-wrapper lannister">
+
+                    <div className="house-picker-wrapper house-stats-wrapper greyjoy">
                         <div className="house-banner">
                             <img className="house-picker-image" src={"/public/media/tag_images/"+Houses[1].image} />
-                            <div className="house-banner-name">House Lannister</div>
+                            <div className="house-banner-name">House Greyjoy</div>
                         </div>
-                        <div className="house-attribute-title">👑 Golden Crown 👑</div>
-                        <div className="house-attribute-detail">+6 for each week Cersei is Queen</div>
-                        <div className="house-attribute-title">👫 Twincest 👫</div>
-                        <div className="house-attribute-detail">-2 for each week Cersei and Jaime don't meet</div>
+                        <div className="house-attribute-title">⚓ We Do Not Sow ⚓</div>
+                        <div className="house-attribute-detail">+10 for each naval battle (2+ ships)</div>
+                        <div className="house-attribute-title">🐙 What is dead, could die 🐙</div>
+                        <div className="house-attribute-detail">-2 For each week no Greyjoy is seen on a ship</div>
                         <div className="house-attribute-stats-container">
                             <div className="house-attribute-title-center-underline">Stats</div>
                             <div className="house-attribute-stats-box">
                                 <div className="house-attribute-stats">
-                                    <div className="house-attribute-stat">+{this.state.houseStats.data[1] && this.state.houseStats.data[1].plusCoins}/-{this.state.houseStats.data[1] && this.state.houseStats.data[1].minusCoins}</div>
+                                    <div className="house-attribute-stat">+{this.state.houseStats.data[1] && this.state.houseStats.data[1].plusCoins}/-{this.state.houseStats.data[0] && this.state.houseStats.data[0].minusCoins}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[1] && this.state.houseStats.data[1].userCount}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[1] && this.state.houseStats.data[1].coins}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[1] && this.state.houseStats.data[1].availableCoins}</div>
@@ -169,20 +170,21 @@ class HouseStatsPage extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="house-picker-wrapper house-stats-wrapper stark">
+
+                    <div className="house-picker-wrapper house-stats-wrapper targaryen">
                         <div className="house-banner">
                             <img className="house-picker-image" src={"/public/media/tag_images/"+Houses[3].image} />
-                            <div className="house-banner-name">House Stark</div>
+                            <div className="house-banner-name">House Targaryen</div>
                         </div>
-                        <div className="house-attribute-title">🗡 Needlework 🗡</div>
-                        <div className="house-attribute-detail">+5 for each Arya kill</div>
-                        <div className="house-attribute-title">🍽 Power Hungry 🍽</div>
-                        <div className="house-attribute-detail">-3 for each week Sansa talks to Littlefinger</div>
+                        <div className="house-attribute-title">🐲 Wild Fire 🐲</div>
+                        <div className="house-attribute-detail">+5 for each dragon flame, cooldown: 1 minute</div>
+                        <div className="house-attribute-title">🍷 Drunken Hand 🍷</div>
+                        <div className="house-attribute-detail">-1 for each wine cup Tyrion drinks</div>
                         <div className="house-attribute-stats-container">
                             <div className="house-attribute-title-center-underline">Stats</div>
                             <div className="house-attribute-stats-box">
                                 <div className="house-attribute-stats">
-                                    <div className="house-attribute-stat">+{this.state.houseStats.data[3] && this.state.houseStats.data[3].plusCoins}/-{this.state.houseStats.data[3] && this.state.houseStats.data[3].minusCoins}</div>
+                                    <div className="house-attribute-stat">+{this.state.houseStats.data[3] && this.state.houseStats.data[3].plusCoins}/-{this.state.houseStats.data[0] && this.state.houseStats.data[0].minusCoins}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[3] && this.state.houseStats.data[3].userCount}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[3] && this.state.houseStats.data[3].coins}</div>
                                     <div className="house-attribute-stat">{this.state.houseStats.data[3] && this.state.houseStats.data[3].availableCoins}</div>
@@ -208,26 +210,27 @@ class HouseStatsPage extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="house-picker-wrapper house-stats-wrapper targaryen">
+
+                    <div className="house-picker-wrapper house-stats-wrapper stark">
                         <div className="house-banner">
                             <img className="house-picker-image" src={"/public/media/tag_images/"+Houses[4].image} />
-                            <div className="house-banner-name">House Targaryen</div>
+                            <div className="house-banner-name">House Stark</div>
                         </div>
-                        <div className="house-attribute-title">🐲 Wild Fire 🐲</div>
-                        <div className="house-attribute-detail">+5 for each dragon flame, cooldown: 1 minute</div>
-                        <div className="house-attribute-title">🍷 Drunken Hand 🍷</div>
-                        <div className="house-attribute-detail">-1 for each wine cup Tyrion drinks</div>
+                        <div className="house-attribute-title">🗡 Needlework 🗡</div>
+                        <div className="house-attribute-detail">+5 for each Arya kill</div>
+                        <div className="house-attribute-title">🍽 Power Hungry 🍽</div>
+                        <div className="house-attribute-detail">-3 for each week Sansa talks to Littlefinger</div>
                         <div className="house-attribute-stats-container">
                             <div className="house-attribute-title-center-underline">Stats</div>
                             <div className="house-attribute-stats-box">
                                 <div className="house-attribute-stats">
-                                    <div className="house-attribute-stat">+{this.state.houseStats.data[0] && this.state.houseStats.data[0].plusCoins}/-{this.state.houseStats.data[0] && this.state.houseStats.data[0].minusCoins}</div>
-                                    <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].userCount}</div>
-                                    <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].coins}</div>
-                                    <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].availableCoins}</div>
-                                    <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].steelWagers}</div>
-                                    <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].ironWagers}</div>
-                                    <div className="house-attribute-stat">{this.state.houseStats.data[0] && this.state.houseStats.data[0].saltWagers}</div>
+                                    <div className="house-attribute-stat">+{this.state.houseStats.data[4] && this.state.houseStats.data[4].plusCoins}/-{this.state.houseStats.data[3] && this.state.houseStats.data[3].minusCoins}</div>
+                                    <div className="house-attribute-stat">{this.state.houseStats.data[4] && this.state.houseStats.data[4].userCount}</div>
+                                    <div className="house-attribute-stat">{this.state.houseStats.data[4] && this.state.houseStats.data[4].coins}</div>
+                                    <div className="house-attribute-stat">{this.state.houseStats.data[4] && this.state.houseStats.data[4].availableCoins}</div>
+                                    <div className="house-attribute-stat">{this.state.houseStats.data[4] && this.state.houseStats.data[4].steelWagers}</div>
+                                    <div className="house-attribute-stat">{this.state.houseStats.data[4] && this.state.houseStats.data[4].ironWagers}</div>
+                                    <div className="house-attribute-stat">{this.state.houseStats.data[4] && this.state.houseStats.data[4].saltWagers}</div>
                                 </div>
                                 <div className="house-attribute-stat-labels">
                                     <div className="house-attribute-stat-label">current bonus</div>
